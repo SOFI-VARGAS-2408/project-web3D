af9b3bf0faaac86a22f030314df4dd3db7cc7b85
Ruben-Torres02 <ruben.torres@correounivalle.edu.co>
2024-08-25 21:05:49 -0500

    Organizar el css de login y como quitar las repetidas entradas del mismo usuario en firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/login/login.jsx'
+import Quiz from './pages/quiz/Quiz.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/quiz',
+    element: <Quiz />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+) 
---
+++ src/pages/quiz/Quiz.jsx
@@ -0,0 +1,21 @@
+import "./Quiz.css";
+import useQuizStore from "../../stores/use-quiz-store"
+import { useCallback } from "react";
+
+const Quiz = () => {
+    const {quiz, incrementQuizProgress} = useQuizStore();
+
+    const onHandleButtonNext = useCallback (()=>{
+        incrementQuizProgress();
+    }, [incrementQuizProgress]);
+  
+    return (
+        <div className = "quiz-container">
+            <h1 className="quiz-header">Hola</h1>
+            <p className="quiz-progress">Progreso del Quiz: {quiz.percentageQuizCompleted}%</p>
+            <button onClick={onHandleButtonNext}>Siguiente</button>
+        </div>
+    )
+}
+
+export default Quiz;
