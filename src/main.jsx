77e044e5ea9d94183f3e1655b6c86449d05eaf80
SOFI-VARGAS-2408 <vargas.sofia@correounivalle.edu.co>
2024-09-07 01:14:26 -0500

    Routes were created to control user access

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/login/login.jsx'
-import Quiz from './pages/quiz/Quiz.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />
-  },
-  {
-    path: '/quiz',
-    element: <Quiz />,
-  },
-]);
+import { RouterProvider } from 'react-router-dom'
+import Router from './routers/Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={Router}/>
   </StrictMode>
 ) 
---
+++ src/routers/Router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Login from '../pages/login/login.jsx'
+import Quiz from '../pages/quiz/Quiz.jsx'
+import Home from '../pages/home/Home.jsx'
+import PublicRoute from './PublicRoute.jsx';
+import ProtectedRoute from './ProtectedRoute.jsx';
+
+const Router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <PublicRoute>
+            <Login />
+        </PublicRoute>
+    )
+    },
+    {
+      path: '/quiz',
+      element: (
+      <ProtectedRoute>
+         <Quiz />
+      </ProtectedRoute>
+      )
+    },
+    {
+      path:'/home',
+      element:(
+        <ProtectedRoute>
+           <Home/>
+        </ProtectedRoute>
+        )
+    }
+  ]);
+
+export default Router;
