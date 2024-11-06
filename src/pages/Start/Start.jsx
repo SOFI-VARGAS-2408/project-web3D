b6aac85bcc912171eadee91502cbbefc6136b212
SOFI-VARGAS-2408 <vargas.sofia@correounivalle.edu.co>
2024-11-06 08:45:40 -0500

    add apge pollution

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -3,6 +3,7 @@ import Button from "../../components/Button/Button"
 import ButtonLogOut from "../../components/ButtonLogOut/ButtonLogOut"
 import "./Start.css"
 import { Trash } from "../../components/Trash/Trash"
+import { OrbitControls } from "@react-three/drei"
 const Start = () =>{
     return (
         <>
@@ -12,8 +13,8 @@ const Start = () =>{
             <div className="container-options-option">
               <Canvas>
                 <ambientLight />
-                <directionalLight position={[10, 10, 10]} intensity={5} />
-                <Trash/>
+                <directionalLight position={[10, 10, 10]} intensity={10} />
+                <Trash position={[-1.5,0,0]} scale={0.8 } rotation={[0,2,0]}/>
               </Canvas>
               <Button text={"Contaminacion"}/>
             </div>
